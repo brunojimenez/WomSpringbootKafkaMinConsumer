5509f7ddbf443e1b1491f319a8be0d11271b8d12
bruno jimenez <bruno.jimenez@wom.cl>
2021-11-17 17:10:21 -0300

    avance

--- src/main/java/cl/wom/poc/WomSpringbootKafkaMinConsumerApplication.java
+++ src/main/java/cl/wom/poc/WomSpringbootKafkaMinConsumerApplication.java
@@ -1,5 +1,9 @@
 package cl.wom.poc;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,7 +33,7 @@ public class WomSpringbootKafkaMinConsumerApplication {
 
 	public static final String TOPIC = "poc-test01";
 
-	public static final String GROUP_ID = "myGroupId01";
+	public static final String GROUP_ID = "myGroupId04";
 
 	public static void main(String[] args) {
 		SpringApplication.run(WomSpringbootKafkaMinConsumerApplication.class, args);
@@ -37,7 +41,7 @@ public class WomSpringbootKafkaMinConsumerApplication {
 
 	@Bean
 	public NewTopic topic() {
-		return TopicBuilder.name("topic1").partitions(10).replicas(1).build();
+		return TopicBuilder.name(TOPIC).partitions(10).replicas(1).build();
 	}
 
 	@Bean
@@ -53,19 +57,35 @@ public class WomSpringbootKafkaMinConsumerApplication {
 		consumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
 		// Kafka broker
-		consumerProps.put("group.id", GROUP_ID);
+		// consumerProps.put("group.id", String.format(GROUP_ID));
 
 		// (boolean - default true)
 		// https://kafka.apache.org/documentation/#consumerconfigs_enable.auto.commit
 		consumerProps.put("enable.auto.commit", false);
 
-		// (int - default 60000 (1 minute))
-		// https://kafka.apache.org/documentation/#consumerconfigs_default.api.timeout.ms
-		consumerProps.put("default.api.timeout.ms", 60000);
+		// Session Timeout (int - default: 45000 (45 seconds))
+		// https://kafka.apache.org/documentation/#consumerconfigs_session.timeout.ms
+		// consumerProps.put("session.timeout.ms", 45000);
 
-		// (int - default 40000 (40 seconds))
+		// Heartbeat Intervals (int - default: 3000 (3 seconds))
+		// https://kafka.apache.org/documentation/#consumerconfigs_heartbeat.interval.ms
+		//consumerProps.put("heartbeat.interval.ms", 1000);
+
+		// Default Api Timeout (int - default 60000 (1 minute))
+		// https://kafka.apache.org/documentation/#consumerconfigs_default.api.timeout.ms
+		// consumerProps.put("default.api.timeout.ms", 60000);
+
+		// Request Timeout (int - default 40000 (40 seconds))
 		// https://kafka.apache.org/documentation/#connectconfigs_request.timeout.ms
-		consumerProps.put("request.timeout.ms", 40000);
+		//consumerProps.put("request.timeout.ms", 130000);
+
+		// Max Poll Interval (int - default: 300000 (5 minutes))
+		// https://kafka.apache.org/documentation/#consumerconfigs_max.poll.interval.ms
+		// consumerProps.put("max.poll.interval.ms", 300000);
+
+		// Max Poll Records (int - default: 500)
+		// https://kafka.apache.org/documentation/#consumerconfigs_max.poll.records
+		consumerProps.put("max.poll.records", 2);
 
 		return new DefaultKafkaConsumerFactory<>(consumerProps, new StringDeserializer(), new StringDeserializer());
 
@@ -76,18 +96,18 @@ public class WomSpringbootKafkaMinConsumerApplication {
 		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
 		factory.setConsumerFactory(consumerFactory());
-		factory.setConcurrency(5);
+		factory.setConcurrency(10);
 		return factory;
 	}
 
-	@KafkaListener(id = "myId", topics = TOPIC, containerFactory = "kafkaListenerContainerFactory")
+	@KafkaListener(id = GROUP_ID, topics = TOPIC, containerFactory = "kafkaListenerContainerFactory")
 	public void listen(ConsumerRecord<String, String> message, Acknowledgment ack) {
 
-		log.info("[runner] value = {}, Partition = {}, Offset = {}", message.value(), message.partition(),
+		log.info("[listen] value = {}, Partition = {}, Offset = {}", message.value(), message.partition(),
 				message.offset());
 
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(120000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
